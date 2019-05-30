c7dcc559b361e3f58d37c8db4101fc5ee9ef2321
Arij El Aini <arijelaini@Arijs-Air.fritz.box>
2019-05-30 16:39:21 +0200

    demo app ready

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const greeting = 'Hello Function Component!';
+
+  return <Headline value={greeting} />;
+}
+
+function Headline(props) {
+  return <h1>{props.value}</h1>;
 }
 
 export default App;
